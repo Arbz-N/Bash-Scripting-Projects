3e2a10535b0cf05f9817fef47180511b792c5f25
Arbaz Naeem <arbaznaeem555@gmail.com>
2025-12-22 23:51:06 +0500

    added readme.md

--- Disk Usage Alert Script/script.sh
+++ Disk Usage Alert Script/script.sh
@@ -8,7 +8,7 @@ THRESHOLD=80
 USAGE=$(df / | grep / | awk '{print $5}' | sed 's/%//')
 
 if [ "$USAGE" -gt "$THRESHOLD" ]; then
-    echo "⚠️ Disk usage high on $(hostname): $USAGE%"
+    echo "Disk usage high on $(hostname): $USAGE%"
 else
     echo "Disk OK: $USAGE%"
 fi
