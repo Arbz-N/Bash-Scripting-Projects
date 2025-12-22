8afae1bff31d4bd228ff3da2d52e58c9b107e27e
Arbaz Naeem <arbaznaeem555@gmail.com>
2025-12-22 23:41:04 +0500

    added script file

---
+++ Disk Usage Alert Script/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=========================================
+# Script: disk_alert.sh
+# Purpose: Send alert if disk usage > 80%
+#=========================================
+
+THRESHOLD=80
+USAGE=$(df / | grep / | awk '{print $5}' | sed 's/%//')
+
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+    echo "⚠️ Disk usage high on $(hostname): $USAGE%"
+else
+    echo "Disk OK: $USAGE%"
+fi
