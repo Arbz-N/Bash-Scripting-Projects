c3dde8fe9c2418008cbffa7590c6fdc2237ffb14
Arbaz Naeem <arbaznaeem555@gmail.com>
2025-12-26 22:35:32 +0500

    update script.sh file

--- Disk Usage Alert Script/script.sh
+++ Disk Usage Alert Script/script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #=========================================
 # Script: disk_alert.sh
-# Purpose: Send alert if disk usage > 80%
+# Purpose: Send email alert if disk usage > 80%
 #=========================================
 
 THRESHOLD=70
+EMAIL="admin@example.com"
+
 USAGE=$(df / | grep / | awk '{print $5}' | sed 's/%//')
 
 if [ "$USAGE" -gt "$THRESHOLD" ]; then
-    echo "Disk usage high on $(hostname): $USAGE%"
-else
-    echo "Disk OK: $USAGE%"
+    if command -v mail &> /dev/null; then
+        echo "Disk usage is above threshold. Current usage is $USAGE%." | mail -s "Disk Usage Alert" "$EMAIL"
+    else
+        echo "Usage is above threshold but mail command is not installed."
+    fi
 fi
